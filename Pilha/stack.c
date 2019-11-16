7cd18b15da0688c4ddbb8bcc43bef96cb2c84d29
Eduardoccufcg <eduardo.pereira.santos@ccc.ufcg.edu.br>
2019-11-16 11:38:58 -0300

    Finalizando pilha estática

--- Pilha/stack.c
+++ Pilha/stack.c
@@ -2,69 +2,87 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct stack
-{
-  int array[MAX];
-  int top;
+struct stack{
+
+	int array[MAX];
+	int top;
 };
 
 void init(struct stack *s){
-  s -> top = -1;
-
+	s->top = -1;
 };
 
 int isEmpty(struct stack *s){
-  if(s -> top == -1){
-    return 1;
-
-  }else{
-    return 0;
-  }
+	if (s->top == -1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
 
 int isFull(struct stack *s){
-  if(s -> top == MAX - 1){
-    return 1;
-  }else{
-    return 0;
-  }
-
+	if (s->top == MAX - 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
+
 int push(struct stack *s, int value){
-  if(!isFull(s)){
-    s -> array[++s -> top] = value;
-    return 0;
-
-  }else{
-    // Está cheia
-    printf("\nStack is full");
-    return 1;
-  }
-
+	if (!isFull(s))
+	{
+		s->array[++s->top] = value;
+		return 0;
+	}
+	else
+	{
+		printf("\nStack is full");
+		return 1;
+	}
 }
+
 int pop(struct stack *s){
-  if(!isEmpty(s)){
-    return s -> array[s ->top--];
-  }else{
-    printf("\nStack is Empty");
-    return -239952128;
-  }
-
-
+	if (!isEmpty(s))
+	{
+		return s->array[s->top--];
+	}
+	else
+	{
+		printf("\nStack is Empty");
+		return -239952128;
+	}
 }
 
-int main (){
+int top(struct stack *s){
+	if (!isEmpty(s))
+	{
+		return s->array[s->top];
+	}
+	else
+	{
+		printf("\nStack is Empty");
+		return -239952128;
+	}
+}
 
-  struct stack *a;
-  a = malloc(sizeof(struct stack));
-  init(a);
-  push(a,2);
-  push(a,3);
-  printf("%i",a -> array[a -> top]);
-  push(a,6);
-  pop(a);
-  pop(a);
-  pop(a);
-  free(a);
-
+int main()
+{
+	struct stack *a;
+	a = malloc(sizeof(struct stack));
+	init(a);
+	push(a, 2);
+	printf("%d", top(a));
+	push(a, 3);
+	printf("\n%i", top(a));
+	push(a, 6);
+	pop(a);
+	pop(a);
+	pop(a);
+	free(a);
 }
